01f5bc016f116b512f47e24370ac39dab53ab6fd
Reid Harry <48355869+Reido50@users.noreply.github.com>
2021-11-03 18:37:58 -0400

    Added orientation examples

--- Project 2/project2.py
+++ Project 2/project2.py
@@ -22,5 +22,39 @@ def DecodeOrientationField(m):
         nextIndex = indexDict[(i+1) % 8]
         theta = m[nextIndex[0]][nextIndex[1]] - m[index[0]][index[1]]
         sumation += PoincareIndex(theta)
-    return sumation / 180
+    result = sumation / 180
+    if result == 0:
+        return "non-singularity"
+    elif result == 1:
+        return "loop"
+    elif result == -1:
+        return "delta"
+    elif result == 2:
+        return "whorl"
+    return None
 
+# Orientation Field Calculations
+# 1
+field1 = [[10, 15, -10],
+            [12, 0, 15],
+            [13, 12, -5]]
+print("Field 1 is a " + DecodeOrientationField(field1))
+# 2
+field2 = [[45, 90, -50],
+          [50, 0, -45],
+          [5, 0, -5]]
+print("Field 2 is a " + DecodeOrientationField(field2))
+# 3
+field3 = [[50, 0, -50],
+          [75, 0, -70],
+          [85, 90, -85]]
+print("Field 3 is a " + DecodeOrientationField(field3))
+# 4
+field4 = [[45, 2, -50],
+          [90, 0, 90],
+          [-50, 2, 50]]
+print("Field 4 is a " + DecodeOrientationField(field4))
+
+# Ridge Pattern Wave
+x = list(range(601))
+y = list(range(601))
